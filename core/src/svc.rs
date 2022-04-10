75717021c8d90ae93cd4112f8e2615e218be76a9
Mikail Bagishov <bagishov.mikail@yandex.ru>
2022-04-10 23:08:54 +0300

    Cleanup imports

--- core/src/svc.rs
+++ core/src/svc.rs
@@ -1,11 +1,7 @@
 use tonic::metadata::MetadataValue;
 
-use crate::auth::{Iam, TokenProviderError};
-use std::{
-    future::Future,
-    pin::Pin,
-    task::{Context, Poll}, sync::Arc,
-};
+use crate::auth::Iam;
+use std::sync::Arc;
 
 /// Interceptor
 pub struct AddToken {
@@ -19,16 +15,17 @@ impl AddToken {
 }
 
 impl tonic::service::Interceptor for AddToken {
-    fn call(&mut self, mut request: tonic::Request<()>) -> Result<tonic::Request<()>, tonic::Status> {
+    fn call(
+        &mut self,
+        mut request: tonic::Request<()>,
+    ) -> Result<tonic::Request<()>, tonic::Status> {
         let token = match self.iam.get() {
             Ok(t) => t,
-            Err(err) => {
-                return Err(tonic::Status::unavailable(format!("{err}")))
-            }
+            Err(err) => return Err(tonic::Status::unavailable(format!("{err}"))),
         };
         let token = match MetadataValue::from_str(token.get()) {
             Ok(mv) => mv,
-            Err(err) => return Err(tonic::Status::internal(format!("invalid IAM token: {err}")))
+            Err(err) => return Err(tonic::Status::internal(format!("invalid IAM token: {err}"))),
         };
         request.metadata_mut().insert("authorization", token);
         Ok(request)
