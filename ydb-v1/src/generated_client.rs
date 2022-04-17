6f004a30aee686391553590693ab40233a0c7f85
Mikail Bagishov <bagishov.mikail@yandex.ru>
2022-04-17 21:40:54 +0300

    Fix formatting

--- ydb-v1/src/generated_client.rs
+++ ydb-v1/src/generated_client.rs
@@ -1,11 +1,10 @@
-use tonic::{transport::Channel, codegen::InterceptedService, transport::Error};
+use tonic::{codegen::InterceptedService, transport::Channel, transport::Error};
 
-use yandex_cloud_api_core::{auth::Iam, svc::AddToken};
 use crate::pb::yandex::cloud::ydb::v1::{
-    backup_service_client::BackupServiceClient,
-    database_service_client::DatabaseServiceClient,
+    backup_service_client::BackupServiceClient, database_service_client::DatabaseServiceClient,
     location_service_client::LocationServiceClient,
 };
+use yandex_cloud_api_core::{auth::Iam, svc::AddToken};
 
 /// Yandex Cloud ydb v1 client
 pub struct YdbV1 {
@@ -15,19 +14,31 @@ pub struct YdbV1 {
 const ENDPOINT: &str = "ydb.api.cloud.yandex.net";
 
 impl YdbV1 {
-pub fn new(iam: Iam) -> Self { Self {iam} }    pub async fn backups(
+    pub fn new(iam: Iam) -> Self {
+        Self { iam }
+    }
+    pub async fn backups(
         &self,
-    ) -> Result<
-BackupServiceClient<InterceptedService<Channel, AddToken>>, Error>
-     {
-    let channel = Channel::from_static(ENDPOINT).connect().await?; let client = BackupServiceClient::with_interceptor(channel, AddToken::new(self.iam.clone()));Ok(client)}    pub async fn databases(
+    ) -> Result<BackupServiceClient<InterceptedService<Channel, AddToken>>, Error> {
+        let channel = Channel::from_static(ENDPOINT).connect().await?;
+        let client =
+            BackupServiceClient::with_interceptor(channel, AddToken::new(self.iam.clone()));
+        Ok(client)
+    }
+    pub async fn databases(
         &self,
-    ) -> Result<
-DatabaseServiceClient<InterceptedService<Channel, AddToken>>, Error>
-     {
-    let channel = Channel::from_static(ENDPOINT).connect().await?; let client = DatabaseServiceClient::with_interceptor(channel, AddToken::new(self.iam.clone()));Ok(client)}    pub async fn locations(
+    ) -> Result<DatabaseServiceClient<InterceptedService<Channel, AddToken>>, Error> {
+        let channel = Channel::from_static(ENDPOINT).connect().await?;
+        let client =
+            DatabaseServiceClient::with_interceptor(channel, AddToken::new(self.iam.clone()));
+        Ok(client)
+    }
+    pub async fn locations(
         &self,
-    ) -> Result<
-LocationServiceClient<InterceptedService<Channel, AddToken>>, Error>
-     {
-    let channel = Channel::from_static(ENDPOINT).connect().await?; let client = LocationServiceClient::with_interceptor(channel, AddToken::new(self.iam.clone()));Ok(client)}}
+    ) -> Result<LocationServiceClient<InterceptedService<Channel, AddToken>>, Error> {
+        let channel = Channel::from_static(ENDPOINT).connect().await?;
+        let client =
+            LocationServiceClient::with_interceptor(channel, AddToken::new(self.iam.clone()));
+        Ok(client)
+    }
+}
